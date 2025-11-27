32ed3829caa800a9a1347ec7879a3a494048c1b2
Hakaisha1 <ixforce4@gmail.com>
2025-11-27 11:49:02 +0700

    Add missing applications to INSTALLED_APPS in settings.py

--- coffeemgds/settings.py
+++ coffeemgds/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer',
+    'employees',
+    'logistic',
+    'report',
 ]
 
 MIDDLEWARE = [
